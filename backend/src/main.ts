bd3b65c20930a26057333d31a444063ff716911f
simbole9314 <simbole9314@gmail.com>
2024-05-07 10:11:01 -0300

    feat(Backend): Api base

---
+++ backend/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 3000; // obtiene el puerto desde las variables de entorno, sino 3000
+  app.setGlobalPrefix('api'); // todas las funciones de la api se les agrega el prefijo /api
+  await app.listen(port);
+  console.log(`api escuchando en http://localhost:${port}/api`);
+}
+bootstrap();
